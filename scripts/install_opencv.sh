3c893ea3748cdeeab4f9ff76fd5c767b170a7744
Lucas Nunno <lucasnunno@gmail.com>
2016-03-11 10:17:00 -0700

    Updated with python install

--- scripts/install_opencv.sh
+++ scripts/install_opencv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# See:
+#	* http://docs.opencv.org/2.4/doc/tutorials/introduction/linux_install/linux_install.html#linux-installation
+
 # Run this script as sudo
 
 # Install cmake.
@@ -13,6 +16,6 @@ git clone https://github.com/Itseez/opencv.git opencv
 cd opencv
 mkdir release 
 cd release
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_NEW_PYTHON_SUPPORT=ON -D BUILD_EXAMPLES=ON ..
+make -j
 make install
