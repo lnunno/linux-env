1ba757b1f2a0cd78479b3588e7a71f5cd1b1b61d
Lucas Nunno <lucasnunno@gmail.com>
2016-03-14 22:01:50 -0600

    Add python installs.

--- scripts/ubuntu15-10/install_dev_tools.sh
+++ scripts/ubuntu15-10/install_dev_tools.sh
@@ -9,4 +9,6 @@ sudo apt-get -y install \
     git                 \
     cmake               \
     zsh                 \
+    python-dev          \
+    python3-dev         \
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
