6fb62a28220cb776aa054850f8be75bae0c6cb6e
Lucas Nunno <lucasnunno@gmail.com>
2016-03-17 17:17:36 -0600

    Update install_dev_tools.sh

--- scripts/ubuntu15-10/install_dev_tools.sh
+++ scripts/ubuntu15-10/install_dev_tools.sh
@@ -15,3 +15,7 @@ sudo apt-get -y install \
 
 # Install oh-my-zsh    
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install fuzzyfinder (fzf)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
