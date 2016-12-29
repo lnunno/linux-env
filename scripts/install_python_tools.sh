887783507f6b825a5075945c436f9a75468c0cca
Lucas Nunno <lucasnunno@gmail.com>
2016-12-29 00:06:18 -0700

    Cleanup repo.

--- scripts/install_python_tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install virtualenv
-
-# create virtual environment
-virtualenv .venv
-source .venv/bin/activate
-
-pip install -r requirements.txt
--- scripts/py_modules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install --upgrade pip
-pip install -r py_modules.txt
